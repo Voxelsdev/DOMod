d0ac630c50f63c5342831423506e34420903b448
SaralynOgden <Saralyn.Ogden@gmail.com>
2016-11-20 10:47:00 -0800

    added forgotten comma

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -26,7 +26,7 @@ module.exports = {
   },
   module: {
     loaders: [{
-      test: /\.js|\.jsx$/
+      test: /\.js|\.jsx$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
     }]
