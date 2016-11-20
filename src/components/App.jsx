a9a54848007874242e06e3cb6139b8115d30da89
SaralynOgden <Saralyn.Ogden@gmail.com>
2016-11-20 11:01:12 -0800

    setup more of webpack

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter } from 'react-router';
+import React, { Component } from 'react';
+
+// import Footer from './Footer';
+// import Header from './Header';
+// import Main from './Main';
+
+class App extends Component {
+  render() {
+
+    return (
+      <BrowserRouter>
+        <div>
+          {/* <Header />
+          <Main />
+          <Footer /> */}
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default App;
