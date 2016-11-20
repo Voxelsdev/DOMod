0f273f0dd34c99eb55829a05501676bc1b82721e
SaralynOgden <Saralyn.Ogden@gmail.com>
2016-11-20 15:05:51 -0800

    unsucessfully worked with codemirror

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 
+// const Codemirror = require('react-codemirror');
+//
+// require('../codemirror/mode/javascript/javascript');
+// require('../codemirror/mode/xml/xml');
+// require('../codemirror/lib/codemirror.css');
+
 class Editor extends Component {
   render() {
-    return (
-      <div>I am the Editor</div>
-    )
+    return <div>I am the editor</div>
   }
 }
 
 export default Editor;
+
+// constructor() {
+//   super();
+//   this.code = '// Code'
+// }
+//
+// updateCode(newCode) {
+//   this.code = newCode;
+// }
+//
+// render() {
+//   const options = {
+//     lineNumbers: true
+//   };
+//
+//   return (
+//     <Codemirror value={this.code}
+//                 onChange={this.updateCode}
+//                 options={options}
+//                 interact={this.interact} />
+//   )
+// }
