3a6461d7caa220326ed85a68fa9e65ad8dd7c8ba
SaralynOgden <Saralyn.Ogden@gmail.com>
2016-11-20 11:17:17 -0800

    fixed App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,16 @@
-import { BrowserRouter } from 'react-router';
 import React, { Component } from 'react';
 
-// import Footer from './Footer';
-// import Header from './Header';
-// import Main from './Main';
+import Header from './Header';
+import Main from './Main';
 
 class App extends Component {
   render() {
 
     return (
-      <BrowserRouter>
-        <div>
-          {/* <Header />
-          <Main />
-          <Footer /> */}
-        </div>
-      </BrowserRouter>
+      <div>
+        <Header />
+        <Main />
+      </div>
     )
   }
 }
