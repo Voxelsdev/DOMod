bd7b1ab1c7f31f26c8044f359dccd8f7531b9f28
Tyler Miller <tylermillerhome@gmail.com>
2016-11-20 11:57:42 -0800

    done did thangs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ class Header extends Component {
   render() {
 
     return (
-      <div>I am the header.</div>
+      <div id="header"></div>
     )
   }
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 
 class Main extends Component {
   render() {
-
     return (
-      <div>I am main.</div>
+      <div className="container">
+        <div className="row">
+          <div className="eight columns">
+
+          </div>
+          <div className='four columns'>
+
+          </div>
+        </div>
+      </div>
     )
   }
 }
