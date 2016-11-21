7f7127257a26053c1854fa4fb9609191a25eea07
Tyler Miller <tylermillerhome@gmail.com>
2016-11-21 15:50:57 -0800

    styling and textarea input

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import classnames from 'classnames';
 import styles from './css/editor';
 
 class Editor extends Component {
+  constructor(props) {
+    super(props);
+    this.submit = this.submit.bind(this);
+  }
+
+  submit() {
+    const value = ReactDOM.findDOMNode(this.refs.htmlinput).value
+    this.props.handleChange(value);
+  }
+
   render() {
     return <div className={styles.editorContainer}>
       <div className="row">
-        <textarea className={classnames(styles.text, 'u-full-width')}>
+        <textarea
+          ref="htmlinput"
+          className={classnames(styles.text, 'u-full-width')}
+        >
         </textarea>
       </div>
       <div className="row">
         <input
           className={classnames('u-full-width', styles.parseHtml)}
-          type="button"
+          type="submit"
+          onClick={this.submit}
           value="Parse HTML!"
         />
       </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,6 +5,21 @@ import DOMviewer from './DOMviewer';
 import styles from './css/main';
 
 class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      html: 'Enter your HTML here!'
+    }
+  }
+
+  handleChange(html) {
+    this.setState({ html });
+  }
+
+  parseHtml() {
+    console.log(this.state.html);
+  }
+
   render() {
     return (
       <div className="row">
@@ -12,7 +27,9 @@ class Main extends Component {
           <DOMviewer />
         </div>
         <div className={classnames('four', 'columns', styles.mainEditContainer)}>
-          <Editor />
+          <Editor
+            hmtl={this.state}
+            handleChange={this.handleChange}/>
         </div>
       </div>
     )
