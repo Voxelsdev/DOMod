126089807957a037ae0adbaf7c2090a99d2a8f87
Tyler Miller <tylermillerhome@gmail.com>
2016-11-23 14:02:24 -0800

    Home page is now /

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Match } from 'react-router';
 import classnames from 'classnames';
 import MainApp from './mainapp';
-// import HomeScreen from './homescreen';
 // import Signup from './signup';
 // import Login from './login';
 
@@ -10,8 +9,7 @@ class Main extends Component {
   render() {
     return (
       <div>
-        {/* <Match pattern="/" component={HomeScreen} /> */}
-        <Match pattern="/viewer" component={MainApp} />
+        <Match pattern="/" component={MainApp} />
         {/* <Match pattern="/login" component={Login} /> */}
         {/* <Match pattern="/signup" component={Signup} /> */}
       </div>
