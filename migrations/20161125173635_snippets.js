66b8f1f7b8d932faeeaad85930379260d6a3759a
Tyler Miller <tylermillerhome@gmail.com>
2016-11-25 22:33:14 -0800

    Tables set up, I`m stupid 🏳

--- migrations/20161125173635_snippets.js
+++ migrations/20161125173635_snippets.js
@@ -1,22 +1,14 @@
 'use strict';
 
 exports.up = function(knex) {
-  return knex.schema.createTable('snippets_users', table => {
+  return knex.schema.createTable('snippets', table => {
     table.increments();
-    table.integer('user_id')
-      .notNullable()
-      .references('id')
-      .inTable('users')
-      .onDelete('CASCADE');
-    table.integer('snippet_id')
-      .notNullable()
-      .references('id')
-      .inTable('snippets')
-      .onDelete('CASCADE');
+    table.text('html').notNullable().defaultTo('<!doctype html>');
+    table.text('javascript').notNullable().defaultTo('console.log("Hello world!")');
     table.timestamps(true, true);
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTable('snippers_users');
+  return knex.schema.dropTable('snippets');
 };
--- migrations/20161125173652_snippets_users.js
+++ migrations/20161125173652_snippets_users.js
@@ -1,12 +1,22 @@
 'use strict';
 
 exports.up = function(knex) {
-  return knex.schema.createTable('snippets', table => {
+  return knex.schema.createTable('snippets_users', table => {
     table.increments();
-    table.string()
+    table.integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE');
+    table.integer('snippet_id')
+      .notNullable()
+      .references('id')
+      .inTable('snippets')
+      .onDelete('CASCADE');
+    table.timestamps(true, true);
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTable('snippets');
+  return knex.schema.dropTable('snippers_users');
 };
