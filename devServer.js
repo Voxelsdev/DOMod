fbe05a8cf736fd2a97a4603284aa9899b952e468
Tyler Miller <tylermillerhome@gmail.com>
2016-11-26 20:08:52 -0800

    Oauth is almost there, can`t tell yet, no wifi 📶🚫

--- devServer.js
+++ devServer.js
@@ -1,10 +1,32 @@
 const path = require('path');
-const express = require('express');
 const webpack = require('webpack');
 const config = require('./webpack.config.dev');
-
+const express = require('express');
+const passport = require('passport');
+require('dotenv').load();
 const app = express();
 const compiler = webpack(config);
+
+app.use(passport.initialize());
+
+passport.use(new GitHubStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: process.env.HOST + "/auth/github/callback",
+}, (accessToken, refreshToken, profile, done) => {
+  User.findOrCreate({ githubId: profile.id }, (err, user) => done(err, user));
+}
+));
+
+passport.serializeUser(function(user, done) {
+  // user has logged in!
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  // user logs out, find the user in the sql db!
+  done(null, user)
+});
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
@@ -22,6 +44,17 @@ app.use(parser);
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+
+app.get('/auth/github',
+  passport.authenticate('github', { scope: [ 'user:email' ] }));
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }), //failure to authenticate
+  function (req,res) {
+    // authenticated! sending to the worky thingy.
+    res.redirect('/');
+  });
 
 app.listen(3000, err => {
   if (err) {
