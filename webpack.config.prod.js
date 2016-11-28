062def7e7fd1966b876d7128fdd0898f700c5e60
Tyler Miller <tylermillerhome@gmail.com>
2016-11-27 17:11:41 -0800

    heroku webpack take 3

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -29,6 +29,25 @@ module.exports = {
       test: /\.js$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
+    }, {
+      test: /\.css/,
+      loader: 'style-loader'
+    }, {
+      test: /\.css$/,
+      loader: 'css-loader',
+      query: {
+        modules: true,
+        localIdentName: '[name]__[local]___[hash:base64:5]'
+      }
+    }, {
+      test: /\.json$/,
+      loader: "json-loader"
+    }, {
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      loaders: [
+        'file?hash=sha512&digest=hex&name=[hash].[ext]',
+        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+      ]
     }]
   }
 };
