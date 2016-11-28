d1e410dca9d290264c7265804717933b3e006b27
Tyler Miller <tylermillerhome@gmail.com>
2016-11-27 16:04:54 -0800

    Fixed 406 finally!!!!!4️⃣0️⃣6️⃣🚫

--- server.js
+++ server.js
@@ -9,47 +9,24 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 const webpack = require('webpack');
 const config = require('./webpack.config.dev');
-const passport = require('passport');
-const GitHubStrategy = require('passport-github2').Strategy;
-require('dotenv').load();
-require('dotenv').config({path: __dirname + '/.env'});
 const port = process.env.PORT || 3000;
 const compiler = webpack(config);
 
+require('dotenv').load();
+require('dotenv').config({path: __dirname + '/.env'});
+
+const passport = require('passport');
+
 app.use(passport.initialize());
+app.use(passport.session());
 
-passport.use(new GitHubStrategy({
-  clientID: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: process.env.HOST + "/auth/github/callback",
-}, (accessToken, refreshToken, profile, done) => {
-  // In a typical application, you would want
-  // to associate the LinkedIn account with a user record in your database,
-  // and return that user instead.
-  // change this to knex
-  knex('users')
-    .select(knex.raw('1=1'))
-    .where('id', profile.id)
-    .then(exists => {
-      if (exists) {
-        // user exists..
-      } else {
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
 
-      }
-    })
-  User.findOrCreate({ githubId: profile.id }, (err, user) => done(err, user));
-}
-));
-
-passport.serializeUser(function(user, done) {        //
-  // user has logged in!                             //
-  done(null, user);                                  //
-});                                                  //
-                                                     // This is where we check our user
-passport.deserializeUser(function(user, done) {      //
-  // user logs out, find the user in the sql db!     //
-  done(null, user)                                   //
-});                                                  //
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
@@ -64,27 +41,22 @@ const parser = require('./src/routes/parser.js');
 
 app.use(parser);
 
-app.use((req, res, next) => {
-  if (/json/.test(req.get('Accept'))) {
-    return next();
-  }
-
-  res.sendStatus(406);
-});
+// CSRF protection
+// app.use((req, res, next) => {
+//   if (/json/.test(req.get('Accept'))) {
+//     return next();
+//   }
+//   console.log('lol csrf');
+//   res.sendStatus(406);
+// });
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/auth/github',
-  passport.authenticate('github', { scope: [ 'user:email' ] }));
+// const auth = require('./src/routes/auth');
 
-app.get('/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/login' }), //failure to authenticate
-  function (req,res) {
-    // authenticated! sending to the worky thingy.
-    res.redirect('/');
-  });
+// app.use('/auth', auth);
 
 app.listen(port, err => {
   if (err) {
---
+++ src/routes/auth.js
@@ -0,0 +1,222 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const knex = require('../../knex');
+const boom = require('boom');
+const { camelizeKeys, decamelizeKeys } = require('humps');
+
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy();
+
+// import CryptoJs from 'crypto-js';
+
+passport.use(new GitHubStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: process.env.HOST + "/auth/github/callback",
+}, (accessToken, refreshToken, profile, done) => {
+  return done(null, { profile, accessToken, refreshToken });
+}));
+
+router.get('/github',
+  passport.authenticate('github', {
+    scope: [ 'user:email' ]
+  }), (req, res) => {
+    res.json(req.user);
+});
+
+router.get('/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }), //failure to authenticate
+  (req, res, next) => {
+    console.log('yo');
+    console.log(req);
+    // authenticated! sending to the worky thingy.
+    res.redirect('/');
+  });
+
+  // router.get('/meetup/callback', passport.authenticate('meetup', {
+  //   failureRedirect: '/login' }), (req, res, next) => {
+  //     const meetupUsername = req.user.profile.username;
+  //     const name = req.user.profile._json.name;
+  //     const providerAvatar = req.user.profile._json.photo.thumb_link;
+  //     const providerId = req.user.profile.id;
+  //     const providerRefToken = req.user.refreshToken;
+  //     const providerType = req.user.profile.provider;
+  //     const providerToken = req.user.accessToken;
+  //     const replitTime = Date.now().toString();
+  //     const hash = CryptoJS.HmacSHA256(replitTime, process.env.REPLIT_KEY);
+  //     const replitHash = CryptoJS.enc.Base64.stringify(hash);
+  //
+  //     const token = {
+  //       msg_mac: replitHash,
+  //       time_created: replitTime
+  //     }
+  //
+  //     knex('users')
+  //       .where('meetup_username', meetupUsername)
+  //       .first()
+  //       .then((result) => {
+  //         let nextUsers = result;
+  //
+  //         if (!result) {
+  //           const newUser = {
+  //             meetupUsername,
+  //             name,
+  //             replitHash,
+  //             replitTime
+  //           };
+  //
+  //           const userRow = decamelizeKeys(newUser);
+  //
+  //           return knex('users').insert(userRow, '*')
+  //             .then((users) => {
+  //               const userId = users[0].id;
+  //
+  //               const defaultSnippets = [
+  //                 {
+  //                   snippet: '',
+  //                   snippetName: 'conditionalConsole',
+  //                   snippetType: 'javascript',
+  //                   lessonName: 'javascript'
+  //                 },
+  //                 {
+  //                   snippet: '\'use strict\';\n\nfunction helloWorld() {\n  return \'Hello world\';\n}\n\nhelloWorld();',
+  //                   snippetName: 'helloWorld',
+  //                   snippetType: 'javascript',
+  //                   lessonName: 'javascript'
+  //                 },
+  //                 {
+  //                   snippet: '/*Functions are a way of telling JavaScript to perform one or many actions.\nWrite a simple function that divides a number by 2.\n\nexample: function divideByTwo() {\n\n  return [Your Code Here]\n}\n\ndivideByTwo()*/',
+  //                   snippetName: 'functionJs',
+  //                   snippetType: 'comment',
+  //                   lessonName: 'javascript'
+  //                 },
+  //                 {
+  //                   snippet: '/*Now, take the function above and add an argument called num to the parameters.\nInstead of dividing a number by two, divide the argument by two.\n\nexample: function divideByTwo(num) {\n\n  return [Your Code Here]\n}\n\ndivideByTwo(64)*/',
+  //                   snippetName: 'functionJsTwo',
+  //                   snippetType: 'comment',
+  //                   lessonName: 'javascript'
+  //                 },
+  //                 {
+  //                   snippet: '/*Numbers in javascript work just like numbers in\nthe real world. Try doing some basic math below.\n\nexample: 1 + 1*/',
+  //                   snippetName: 'numberJs',
+  //                   snippetType: 'comment',
+  //                   lessonName: 'javascript'
+  //                 },
+  //                 {
+  //                   snippet: '/*In JavaScript, code written inside of quotes is called a string.\nType your name in quotes, then type a semi-colon.\n\nexample: \'Bill Murray\';*/',
+  //                   snippetName: 'stringsJs',
+  //                   snippetType: 'comment',
+  //                   lessonName: 'javascript'
+  //                 },
+  //                 {
+  //                   snippet: '/*Variables are places where you can store information.\nYou declare a variable using the keyword, var.\nTry storing a string in a variable.\n\nexample: var greeting = \'Hello World\';\n\ngreeting;*/',
+  //                   snippetName: 'varJs',
+  //                   snippetType: 'comment',
+  //                   lessonName: 'javascript'
+  //                 }
+  //               ];
+  //
+  //               for (const snippet of defaultSnippets) {
+  //                 snippet.userId = userId;
+  //                 const row = decamelizeKeys(snippet);
+  //
+  //                 knex('snippets').insert(row, '*')
+  //                   .catch((err) => {
+  //                     next(err);
+  //                   });
+  //               }
+  //
+  //               return users;
+  //             });
+  //         }
+  //
+  //         const updateUser = {
+  //           replitHash,
+  //           replitTime
+  //         };
+  //
+  //         const row = decamelizeKeys(updateUser);
+  //
+  //         knex('users')
+  //           .update(row, '*')
+  //           .where('meetup_username', meetupUsername)
+  //           .then((users) => {
+  //             return nextUsers = users;
+  //           })
+  //           .catch((err) => {
+  //             next(err);
+  //           });
+  //
+  //         return nextUsers;
+  //       })
+  //       .then((users) => {
+  //         if (users[0]) {
+  //           const newIdentity = {
+  //             providerAvatar,
+  //             providerId,
+  //             providerRefToken,
+  //             providerType,
+  //             providerToken,
+  //             userId: users[0].id
+  //           }
+  //
+  //           const identityRow = decamelizeKeys(newIdentity);
+  //
+  //           return knex('identities').insert(identityRow, '*');
+  //         }
+  //
+  //         return knex('identities')
+  //           .where('user_id', users.id)
+  //           .first()
+  //           .then((users) => {
+  //             if (users) {
+  //               const updateIdentity = {
+  //                 providerAvatar,
+  //                 providerId,
+  //                 providerRefToken,
+  //                 providerToken
+  //               }
+  //
+  //               const updateRow = decamelizeKeys(updateIdentity);
+  //
+  //               return knex('identities')
+  //                 .update(updateRow, '*')
+  //                 .where('provider_id', providerId);
+  //             }
+  //           });
+  //       })
+  //       .then((identities) => {
+  //         res.cookie('providerId', identities[0].provider_id);
+  //         res.cookie('userId', identities[0].user_id);
+  //         res.cookie('loggedIn', 'true');
+  //         res.redirect('/');
+  //       })
+  //       .catch((err) => {
+  //         next(err);
+  //       });
+  // });
+  //
+  // router.get('/meetup/events', (req, res, next) => {
+  //   request('https://api.meetup.com/Learn-Code-Seattle/events?&sign=true&photo-host=public&page=2')
+  //     .then((events) => {
+  //       res.send(events)
+  //     })
+  //     .catch((err) => {
+  //       next(err);
+  //     });
+  // });
+  //
+  // router.get('/logout', (req, res) => {
+  //   req.logout();
+  //   res.clearCookie('providerId');
+  //   res.clearCookie('userId');
+  //   res.clearCookie('loggedIn');
+  //   res.redirect('/');
+  // });
+  //
+  //
+  // module.exports = router;
+module.exports = router;
