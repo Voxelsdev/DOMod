2de349576e535ec6d7b9767bf593078c8cdea272
Tyler Miller <tylermillerhome@gmail.com>
2016-11-28 20:12:24 -0800

    redid navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,24 +4,31 @@ import classnames from 'classnames';
 import styles from './css/header';
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.openBracket = '<';
+    this.closingBracket = '/>';
+  }
+
   render() {
     return (
       <header>
         <div
-          className={classnames(styles.navbar, 'row')}
-          id={styles.navbar}
-        >
-          <div className="three columns">
-            <p id={styles.title}><Link  to="/"
-                                        style={{
-                                          color: '#373737',
-                                          textDecoration: 'none',
-                                          textTransform: 'uppercase',
-                                        }}>
-                                        DOMod</Link></p>
+          className={classnames(styles.navbar, 'row')}>
+          <div className="four columns offset-by-four" id={styles.titleContainer}>
+            <p id={styles.title}>
+              <Link  to="/"
+              style={{
+                color: '#373737',
+                textDecoration: 'none',
+                textTransform: 'uppercase',
+              }}>
+              <span id={styles.openBracket}>{this.openBracket} </span>DOMod <span id={styles.closingBracket}>/></span></Link>
+            </p>
           </div>
-
-          <div className="two columns login offset-by-seven">
+        </div>
+        <div className={classnames(styles.navbar, 'row')}>
+          <div className="two columns login">
             <Link to="/login">
               <button className={classnames('u-full-width', styles.login)}>
                 Login
