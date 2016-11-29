01d4082aa64a2574be6779bbc9dc16d9a87050b3
Tyler Miller <tylermillerhome@gmail.com>
2016-11-29 10:22:29 -0800

    Login -> Log

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Match } from 'react-router';
 import classnames from 'classnames';
 import Main from './Main';
-import Login from './Login';
+import Login from './Log';
 
 class Router extends Component {
   render() {
