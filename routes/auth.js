12a625ac7efb69c2f9d4f6e580755322c6a61a4a
Tyler Miller <tylermillerhome@gmail.com>
2016-11-29 13:54:44 -0800

    Oauth works!!!

--- routes/auth.js
+++ routes/auth.js
@@ -15,7 +15,7 @@ const GitHubStrategy = require('passport-github2').Strategy;
 passport.use(new GitHubStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: process.env.HOST + "/auth/github/callback",
+  callbackURL: 'https://dommodded.herokuapp.com/auth/github/callback',
   scope: [ 'user:email' ],
 }, (accessToken, refreshToken, profile, done) => {
   return done(null, { profile, accessToken, refreshToken });
@@ -28,28 +28,28 @@ router.get('/github',
 
 router.get('/github/callback',
   passport.authenticate('github',
-    { failureRedirect: '/login', successRedirect: '/' }),
+    { failureRedirect: '/login', successRedirect: '/' }), //failure to authenticate
     (req, res, next) => {
       const userEmail = req.user.email;
-
-      knex('users')
-      .select(knex.raw('1=1'))
-      .where('email', userEmail)
-      .then(result => {
-        if (!result) {
-          // creates a new user!
-          const newUser = {
-            userEmail,
-          }
-
-          knex('users').insert(decamelizeKeys(newUser), '*')
-          .then(users => {
-            return users;
-          }).catch((err) => {
-            next(err);
-          });
-        }
-      })
+      console.log(userEmail);
+      // knex('users')
+      // .select(knex.raw('1=1'))
+      // .where('email', userEmail)
+      // .then(result => {
+      //   if (!result) {
+      //     // creates a new user!
+      //     const newUser = {
+      //       userEmail,
+      //     }
+      //
+      //     knex('users').insert(decamelizeKeys(newUser), '*')
+      //     .then(users => {
+      //       return users;
+      //     }).catch((err) => {
+      //       next(err);
+      //     });
+      //   }
+      // })
     });
 
 module.exports = router;
--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -10,7 +10,7 @@ class Login extends Component {
   }
 
   handleLogin() {
-    this.props.handleLoginState(true);
+    // this.props.handleLoginState(true);
     window.location.href='/auth/github';
   }
 
